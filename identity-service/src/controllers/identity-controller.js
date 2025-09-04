d03d2b98d12ba35c6f54bd08c483055c90896466
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-09-04 11:12:46 +0100

    Implement user login, refresh token generation, and logout functionality

--- identity-service/src/controllers/identity-controller.js
+++ identity-service/src/controllers/identity-controller.js
@@ -1,7 +1,8 @@
 const logger = require('../utils/logger');
-const { validateRegistration } = require('../utils/validation');
+const { validateRegistration, validateLogin } = require('../utils/validation');
 const User = require('../models/User');
 const generateTokens = require('../utils/generateToken');
+const RefreshToken = require('../models/RefreshToken');
 
 // User Registration
 const registerUser = async (req, res) => {
@@ -49,9 +50,136 @@ const registerUser = async (req, res) => {
 };
 
 // User Login
+const loginUser = async (req, res) => {
+	logger.info('Login endpoint hit...');
+	try {
+		const { error } = validateLogin(req.body);
+
+		if (error) {
+			logger.warn('Validation error', error.details[0].message);
+			return res.status(400).json({
+				success: false,
+				message: error.details[0].message,
+			});
+		}
+
+		const { email, password } = req.body;
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			logger.warn('Invalid user');
+			return res.status(400).json({
+				success: false,
+				message: 'Invalid user',
+			});
+		}
+
+		// Validate password
+		const isValidPassword = await user.comparePassword(password);
+		if (!isValidPassword) {
+			logger.warn('Invalid password');
+			return res.status(400).json({
+				success: false,
+				message: 'Invalid password',
+			});
+		}
+
+		const { accessToken, refreshToken } = await generateTokens(user);
+
+		res.json({
+			accessToken,
+			refreshToken,
+			userId: user._id,
+		});
+	} catch (error) {
+		logger.error('Login error occured', error);
+		return res.status(500).json({
+			success: false,
+			message: 'Internal server error',
+		});
+	}
+};
 
 // Refresh Token Generation
+const refreshTokenGenerator = async (req, res) => {
+	logger.info('Refresh token endpoint hit...');
+	try {
+		const { refreshToken } = req.body;
+		if (!refreshToken) {
+			logger.warn('Refresh token not provided');
+			return res.status(400).json({
+				success: false,
+				message: 'Refresh token not provided',
+			});
+		}
+
+		const storedToken = await RefreshToken.findOne({ token: refreshToken });
+
+		if (!storedToken || storedToken.expiresAt < new Date()) {
+			logger.warn('Invalid or expired refresh token');
+
+			return res.status(401).json({
+				success: false,
+				message: 'Invalid or expired refresh token',
+			});
+		}
+
+		const user = await User.findById(storedToken.user);
+		if (!user) {
+			logger.warn('User not found');
+
+			return res.status(401).json({
+				success: false,
+				message: 'User not found',
+			});
+		}
+
+		const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+			await generateTokens(user);
+
+		// Delete the old refresh token
+		await RefreshToken.deleteOne({ _id: storedToken._id });
+
+		res.json({
+			accessToken: newAccessToken,
+			refreshToken: newRefreshToken,
+		});
+	} catch (error) {
+		logger.error('Refresh token error occured', error);
+		return res.status(500).json({
+			success: false,
+			message: 'Internal server error',
+		});
+	}
+};
 
 // Logout
+const logoutUser = async (req, res) => {
+	logger.info('Logout endpoint hit...');
+	try {
+		const { refreshToken } = req.body;
+		if (!refreshToken) {
+			logger.warn('Refresh token not provided');
+			return res.status(400).json({
+				success: false,
+				message: 'Refresh token not provided',
+			});
+		}
 
-module.exports = { registerUser };
+		await RefreshToken.deleteOne({ token: refreshToken });
+		logger.info('Refresh token deleted successfully for logout');
+
+		res.json({
+			success: true,
+			message: 'Logged out successfully',
+		});
+	} catch (error) {
+		logger.error('Error logging out', error);
+		return res.status(500).json({
+			success: false,
+			message: 'Internal server error',
+		});
+	}
+};
+
+module.exports = { registerUser, loginUser, refreshTokenGenerator, logoutUser };
--- identity-service/src/utils/validation.js
+++ identity-service/src/utils/validation.js
@@ -12,4 +12,13 @@ const validateRegistration = (data) => {
 	return schema.validate(data);
 };
 
-module.exports = { validateRegistration };
+const validateLogin = (data) => {
+	const schema = joi.object({
+		email: joi.string().email().required(),
+		password: joi.string().min(6).required(),
+	});
+
+	return schema.validate(data);
+};
+
+module.exports = { validateRegistration, validateLogin };
